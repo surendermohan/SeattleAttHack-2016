64ab35f0c9a13ca73cf1ad67a29c8a9c5da27bbc
Matthew Calligaro <mcalligaro@pioneer-inc.com>
2016-07-30 13:59:07 -0700

    Lots of changes to the main console application, Walmart API not completely working yet

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,20 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
 namespace ConsoleApplication1
 {
     class Program
@@ -33,7 +47,7 @@ namespace ConsoleApplication1
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
+                //Console.WriteLine("Credential file saved to: " + credPath);
             }
 
             // Create Gmail API service.
@@ -44,17 +58,42 @@ namespace ConsoleApplication1
             });
 
             UsersResource.MessagesResource.ListRequest messagesRequest = service.Users.Messages.List("me");
-            messagesRequest.Q = "matthewcalligaro@hotmail.com";
+            messagesRequest.Q = "from:matthewcalligaro@hotmail.com";
             IList<Message> messages = messagesRequest.Execute().Messages;
+
+            //for (int i = 0; i < messages.Count; i++)
+            //{
+            //    UsersResource.MessagesResource.GetRequest messageRequest = service.Users.Messages.Get("me", messages[i].Id);
+            //    messageRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Minimal;
+            //    Message message = messageRequest.Execute();
+
+            //    //Console.WriteLine(i + ": " + message.Snippet);
+            //}
 
             for (int i = 0; i < messages.Count; i++)
             {
                 UsersResource.MessagesResource.GetRequest messageRequest = service.Users.Messages.Get("me", messages[i].Id);
-                messageRequest.Format = UsersResource.MessagesResource.GetRequest.FormatEnum.Minimal;
                 Message message = messageRequest.Execute();
 
-                Console.WriteLine(message.Snippet);
+                string messageBody = GetMimeString(message.Payload);
+                messageBody = messageBody.Replace("\r", "");
+                messageBody = messageBody.Replace("\n", "");
+                for (int j = 0; j < 9; j++)
+                {
+                    messageBody = messageBody.Replace("  ", " ");
+                }
+
+                Console.WriteLine(messageBody);
                 Console.WriteLine();
+                Console.WriteLine();
+
+                string price = messageBody.Substring(messageBody.IndexOf("Order total: $") + 14, messageBody.IndexOf("Billing information") - messageBody.IndexOf("Order total: $") - 14);
+                Console.WriteLine("Price: " + price);
+
+                string itemName = messageBody.Substring(messageBody.IndexOf("Price Total ") + 12, messageBody.IndexOf("$", messageBody.IndexOf("Price Total ")) - messageBody.IndexOf("Price Total ") - 15);
+                Console.WriteLine("Item Name: " + itemName);
+
+                GetUPC(itemName);
             }
 
 
@@ -62,5 +101,41 @@ namespace ConsoleApplication1
             Console.Read();
         }
 
+
+        public static String GetMimeString(MessagePart Parts)
+        {
+            String Body = "";
+
+            if (Parts.Parts != null)
+            {
+                foreach (MessagePart part in Parts.Parts)
+                {
+                    Body = String.Format("{0}\n{1}", Body, GetMimeString(part));
+                }
+            }
+            else if (Parts.Body.Data != null && Parts.Body.AttachmentId == null && Parts.MimeType == "text/plain")
+            {
+                String codedBody = Parts.Body.Data.Replace("-", "+");
+                codedBody = codedBody.Replace("_", "/");
+                byte[] data = Convert.FromBase64String(codedBody);
+                Body = Encoding.UTF8.GetString(data);
+            }
+
+            return Body;
+        }
+
+        //Take the product name and find the item's upc
+        public static void GetUPC(String productName)
+        {
+            WebClient webclient = new WebClient();
+            string formatedName = productName.Replace(" ", "%20");
+
+            string result = webclient.DownloadString("http://api.walmartlabs.com/v1/search?apiKey=28npz4h9tt2pmgmkh6fse5tr&query=" + formatedName);
+
+            Console.WriteLine(result.IndexOf("upc\":\""));
+            //string upc = result.Substring(result.IndexOf("upc\":\"") + 6, result.IndexOf("\"", result.IndexOf("upc\":\"")) - result.IndexOf("upc\":\"") - 6);
+
+            //Console.WriteLine(upc);
+        }
     }
 }
